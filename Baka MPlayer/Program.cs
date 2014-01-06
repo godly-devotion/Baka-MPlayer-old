e7ce185ff0c5c45b56e3dfa583befcb7c3e3d8a4
azian.otaku@live <azian.otaku@live.com>
2014-01-05 21:29:36 -0500

    Program.cs: create 'error_info.txt' file on exception

--- Baka MPlayer/Program.cs
+++ Baka MPlayer/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Baka_MPlayer.Forms;
+using System;
+using System.Text;
+using System.Threading;
 using System.Windows.Forms;
-using Baka_MPlayer.Forms;
 
 namespace Baka_MPlayer
 {
@@ -14,9 +14,69 @@ namespace Baka_MPlayer
         [STAThread]
         static void Main()
         {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            Application.ThreadException += ApplicationThreadException;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        /// <summary>
+        /// Returns version information (1.2.3.4)
+        /// </summary>
+        public static string GetVersion()
+        {
+            return Application.ProductVersion;
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                var ex = (Exception)e.ExceptionObject;
+                DumpData(ex.Message, ex.StackTrace);
+                MessageBox.Show("Baka MPlayer ran into a fatal problem!\n" +
+                                "Information about the error has been saved to \'error_info.txt\'",
+                                "Uh oh", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            catch (Exception) { }
+            finally
+            {
+                Application.Exit();
+            }
+        }
+
+        public static void ApplicationThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            try
+            {
+                DumpData(e.Exception.Message, e.Exception.StackTrace);
+                MessageBox.Show("Baka MPlayer ran into a problem it couldn't handle!\n" +
+                                "Information about the error has been saved to \'error_info.txt\'",
+                                "Uh oh", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            catch (Exception) { }
+        }
+
+        public static void DumpData(string msg, string stackTrace)
+        {
+            using (var file = new System.IO.StreamWriter("error_info.txt", true))
+            {
+                var contents = new StringBuilder();
+                contents.AppendLine("Baka MPlayer Error Info");
+                contents.AppendFormat("Version: {0}\n\n", GetVersion());
+
+                contents.AppendFormat("Generated: {0}\n", DateTime.UtcNow);
+                contents.AppendFormat("OSVersion: {0}\n", Environment.OSVersion);
+                contents.AppendFormat("Is64BitOperatingSystem: {0}\n", Functions.OS.IsRunning64Bit());
+                contents.AppendFormat("Is64BitProcess: {0}\n\n", "n/a");
+
+                contents.AppendFormat("Message: {0}\n", msg);
+                contents.AppendFormat("Stack Trace:\n{0}\n", stackTrace);
+                contents.AppendLine("--------------------------------------------------");
+
+                file.WriteLine(contents);
+            }
         }
     }
 }
--- Baka MPlayer/Properties/AssemblyInfo.cs
+++ Baka MPlayer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Joshua Park")]
 [assembly: AssemblyProduct("Baka MPlayer")]
-[assembly: AssemblyCopyright("Copyright © 2013")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
