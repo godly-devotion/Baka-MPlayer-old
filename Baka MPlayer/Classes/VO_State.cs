d3c73ca16a32de20913fb441cf5ba16aa90c5a5c
azian.otaku@live <azian.otaku@live.com>
2014-02-07 14:54:06 -0500

    VO_State.cs: fix comments

--- Baka MPlayer/Classes/VO_State.cs
+++ Baka MPlayer/Classes/VO_State.cs
@@ -7,12 +7,12 @@ public static class VO_State
     public static Point LastCursorPos = new Point(1, 1);
 
     /// <summary>
-    /// Used as the global bool for full screen
+    /// Global bool identfier for full screen (does not set actual fullscreen state)
     /// </summary>
     public static bool FullScreen { get; set; }
 
     /// <summary>
-    /// Interal governer for mplayerPanel's display aspect ratio
+    /// Internal governer for mplayerPanel's display aspect ratio
     /// </summary>
     public static double PanelAspectRatio { get; set; }
 }
